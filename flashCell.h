5985d51c3bc2e3c6eaa4438702a7847c171943ce
Pascal Pieper <Pascal.Pieper@dlr.de>
2017-04-03 08:24:23 +0000

    Latest results and Bugfix

--- flashCell.h
+++ flashCell.h
@@ -62,7 +62,7 @@ public:
 			latch();
 		}
 		if(radiation_dose > failpoint.rad * TID_FLIP_START_PERCENT){
-			if(rand() % static_cast<int>((100* failpoint.rad * (1 - TID_FLIP_START_PERCENT)) < radiation_dose - failpoint.rad * TID_FLIP_START_PERCENT)){
+			if(rand() % static_cast<int>(100* failpoint.rad * (1 - TID_FLIP_START_PERCENT)) < radiation_dose - failpoint.rad * TID_FLIP_START_PERCENT){
 				//Bei erreichen der TID -> geringe Wahrscheinlichkeit eines FLIPS
 				flipBit();
 			}
